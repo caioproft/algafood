b06bcec0654634b9e9a1a85188d17dc15b040212
caioproft <caio_proft@hotmail.com>
2021-09-25 23:53:17 -0300

    Adiciona script para popular tabelas

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into cozinha (id, nome) values (1, 'Brasileira');
+insert into cozinha (id, nome) values (2, 'Italiana');
+insert into cozinha (id, nome) values (3, 'Japonesa');
+
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Sabor Brasil', 5.00, 1);
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Massa Nostra', 6.50, 2);
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Ki', 10.00, 3);
