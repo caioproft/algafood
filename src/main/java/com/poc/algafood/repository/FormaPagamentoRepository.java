40f1ba2d5e16ec3fab54c28d0b1013ad4c3d8109
caioproft <caio_proft@hotmail.com>
2021-09-25 22:27:40 -0300

    Adiciona novas entidades e repositórios

---
+++ src/main/java/com/poc/algafood/repository/FormaPagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.poc.algafood.repository;
+
+import com.poc.algafood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+}
