3b82866e67d0d7248c7315325b9669e06406cf30
Caio Proft <caio_proft@hotmail.com>
2021-11-30 23:48:46 -0300

    Adiciona novos testes de integração

--- src/test/java/com/poc/algafood/service/CozinhaServiceTest.java
+++ src/test/java/com/poc/algafood/service/CozinhaServiceTest.java
@@ -1,27 +1,51 @@
 package com.poc.algafood.service;
 
 import com.poc.algafood.domain.model.Cozinha;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.poc.algafood.exception.EntidadeJaCadastradaException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CozinhaServiceTest {
 
-    @Autowired
-    private CozinhaService cozinhaService;
+  @Autowired private CozinhaService cozinhaService;
 
-    @Test
-    public void deveCriarCozinha() {
+  @Test
+  public void deveCriarCozinhaComSucesso() {
 
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(1L);
-        cozinha.setNome("Chinesa");
-        Cozinha cozinhaCriada = cozinhaService.criar(cozinha);
+    Cozinha cozinha = new Cozinha();
+    cozinha.setNome("Chinesa");
+    Cozinha cozinhaCriada = cozinhaService.criar(cozinha);
 
-        Assertions.assertEquals(cozinha.getNome(), cozinhaCriada.getNome());
-        Assertions.assertEquals(cozinha.getId(), cozinhaCriada.getId());
+    assertThat(cozinhaCriada).isNotNull();
+    assertThat(cozinhaCriada.getId()).isNotNull();
+    assertThat(cozinhaCriada.getNome()).isNotNull();
+  }
 
-    }
+  @Test(expected = DataIntegrityViolationException.class)
+  public void deveFalharAoCadastrarCozinhaSemNome() {
+
+    Cozinha cozinha = new Cozinha();
+    cozinha.setNome(null);
+    cozinhaService.criar(cozinha);
+  }
+
+  @Test(expected = EntidadeJaCadastradaException.class)
+  public void deveFalharAoCadastrarCozinhaComNomeJaCadastrado() {
+
+    Cozinha cozinha = new Cozinha();
+    cozinha.setNome("Brasileira");
+    cozinhaService.criar(cozinha);
+
+    Cozinha cozinhaComMesmoNome = new Cozinha();
+    cozinhaComMesmoNome.setNome("Brasileira");
+    cozinhaService.criar(cozinhaComMesmoNome);
+  }
 }
