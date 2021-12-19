9882d201553f1fb93c0cd9f836bdfcebec082518
idd_cproft <idd_cproft@uolinc.com>
2021-12-18 22:20:30 -0300

    Refatora CRUD

--- src/main/java/com/poc/algafood/service/CozinhaService.java
+++ src/main/java/com/poc/algafood/service/CozinhaService.java
@@ -15,64 +15,59 @@ import java.util.Optional;
 @Service
 public class CozinhaService {
 
-  private CozinhaRepository repository;
+    private CozinhaRepository repository;
 
-  public CozinhaService(CozinhaRepository repository) {
-    this.repository = repository;
-  }
-
-  public List<Cozinha> buscarTodas() {
-    return repository.findAll();
-  }
-
-  public Optional<Cozinha> buscarUma(Long id) {
-    return repository.findById(id);
-  }
-
-  public Cozinha criar(Cozinha cozinha) {
-
-    if (verificaSeJaExisteCozinhaPorNome(cozinha)) {
-      throw new EntidadeJaCadastradaException(
-          String.format("A cozinha %s já está cadastrada no sistema", cozinha.getNome()));
-    } else {
-      return repository.save(cozinha);
-    }
-  }
-
-  public Optional<Cozinha> atualizar(Long id, Cozinha cozinha) {
-    Optional<Cozinha> cozinhaExistente = repository.findById(id);
-
-    if (cozinhaExistente.isPresent()) {
-      BeanUtils.copyProperties(cozinha, cozinhaExistente, "id");
-      repository.save(cozinhaExistente.get());
-      return cozinhaExistente;
+    public CozinhaService(CozinhaRepository repository) {
+        this.repository = repository;
     }
 
-    throw new EntidadeNaoCadastradaException(
-        String.format("Cozinha de ID = %d não cadastrada no sistema", id));
-  }
+    private static String COZINHA_NÂO_ENCONTRADA = "Não existe uma cozinha cadastrada com o ID = %d";
+    private static String COZINHA_EM_USO = "A cozinha de código %d não pode ser excluída, pois está em uso por algum restaurante";
 
-  public void excluir(Long id) {
 
-    try {
-      Optional<Cozinha> cozinha = repository.findById(id);
-
-      if (cozinha.isPresent()) {
-        repository.delete(cozinha.get());
-      } else {
-        throw new EntidadeNaoCadastradaException("Cozinha não cadastrada");
-      }
-    } catch (DataIntegrityViolationException ex) {
-
-      throw new EntidadeEmUsoException(
-          String.format(
-              "A cozinha de código %d não pode ser excluída, pois está em uso por algum restaurante",
-              id));
+    public List<Cozinha> buscarTodas() {
+        return repository.findAll();
     }
-  }
 
-  private boolean verificaSeJaExisteCozinhaPorNome(Cozinha cozinha) {
-    Optional<Cozinha> resultado = repository.findByNome(cozinha.getNome());
-    return resultado.isPresent();
-  }
+    public Optional<Cozinha> buscarUma(Long id) {
+        return repository.findById(id);
+    }
+
+    public Cozinha criar(Cozinha cozinha) {
+
+        if (verificaSeJaExisteCozinhaPorNome(cozinha)) {
+            throw new EntidadeJaCadastradaException(
+                    String.format("A cozinha %s já está cadastrada no sistema", cozinha.getNome()));
+        } else {
+            return repository.save(cozinha);
+        }
+    }
+
+    public Cozinha atualizar(Long id, Cozinha cozinha) {
+        Cozinha cozinhaExistente = repository.findById(id).orElseThrow(() -> new EntidadeNaoCadastradaException(String.format(COZINHA_NÂO_ENCONTRADA, id)));
+
+        BeanUtils.copyProperties(cozinha, cozinhaExistente, "id");
+        repository.save(cozinhaExistente);
+        return cozinhaExistente;
+
+    }
+
+    public void excluir(Long id) {
+
+        try {
+            Cozinha cozinha = repository.findById(id).orElseThrow(() -> new EntidadeNaoCadastradaException(String.format(COZINHA_NÂO_ENCONTRADA, id)));
+
+            repository.delete(cozinha);
+
+        } catch (DataIntegrityViolationException ex) {
+
+            throw new EntidadeEmUsoException(String.format(COZINHA_EM_USO, id));
+        }
+
+    }
+
+    private boolean verificaSeJaExisteCozinhaPorNome(Cozinha cozinha) {
+        Optional<Cozinha> resultado = repository.findByNome(cozinha.getNome());
+        return resultado.isPresent();
+    }
 }
